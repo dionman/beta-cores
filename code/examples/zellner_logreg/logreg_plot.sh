d5819509967f7cdea86a5319bffe6462e250f9ad
DM <d.manousakas@gmail.com>
2020-07-22 11:42:20 +0300

    logreg plots and eval small edits

--- code/examples/zellner_logreg/logreg_plot.sh
+++ code/examples/zellner_logreg/logreg_plot.sh
@@ -2,28 +2,57 @@
 
 fldr_figs='figs'
 fldr_res='results'
-beta="0.01"
-i0="1."
-f_rate="15"
-graddiag="False"
-for dnm in 'adult' #'santa100K' 'webspam'
+for beta in  "0.01"
+do
+for i0 in "1.0"
+do
+for f_rate in "0"
+do
+for graddiag in "False"
+do
+for dnm in 'adult'
   do
+    #echo $dnm $fldr_figs $fldr_res $beta $i0 $f_rate $graddiag
     python3 plot.py $dnm $fldr_figs $fldr_res $beta $i0 $f_rate $graddiag
   done
+done
+done
+done
+done
 
 
 '''
-santa100K
---------------
-beta="0.01"
-i0="1."
-f_rate="30"
-graddiag="False"
+fldr_figs='figs'
+fldr_res='results'
+for beta in "0.01" "0.01" "0.5" "0.9"
+do
+for i0 in "0.1" "1." "10."
+do
+for f_rate in "0" "15" "30"
+do
+for graddiag in "False" "True"
+do
+for dnm in 'webspam' 'adult' 'santa100K'
+  do
+    #echo $dnm $fldr_figs $fldr_res $beta $i0 $f_rate $graddiag
+    python3 plot.py $dnm $fldr_figs $fldr_res $beta $i0 $f_rate $graddiag
+  done
+done
+done
+done
+done
+'''
 
-adult
----------------
-beta="0.01"
-i0="1."
-f_rate="15"
-graddiag="False"
+'''
+str(beta)+'_'+str(i0)+'_'+str(f_rate)+'_'+str(graddiag)
+
+adult0.5_0.1_15_False_ACCvssz.png
+adult0.5_1.0_30_False_ACCvssz.png
+adult0.9_1.0_30_False_ACCvssz.png
+
+
+santa100K0.01_10.0_15_False_ACCvssz.png
+santa100K0.01_10.0_30_False_ACCvssz.png
+santa100K0.5_10.0_15_True_ACCvssz.png
+santa100K0.5_10.0_30_True_ACCvssz.png
 '''
--- code/examples/zellner_logreg/plot.py
+++ code/examples/zellner_logreg/plot.py
@@ -7,8 +7,15 @@ import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '../common'))
 from plotting import *
 import bokeh.layouts as bkl
+from bokeh.models import Title
 
 dnm=sys.argv[1]
+if dnm=='adult':
+  dnmnm='ADULT'
+elif dnm=='santa100K':
+  dnmnm='TRANSACTIONS'
+else:
+  dnmnm='WEBSPAM'
 fldr_figs=sys.argv[2]
 fldr_res=sys.argv[3]
 beta=float(sys.argv[4])
@@ -16,27 +23,29 @@ i0=float(sys.argv[5])
 f_rate=int(sys.argv[6])
 graddiag=str(sys.argv[7])
 
-algs = [('BCORES', 'β-Cores', pal[0]), ('BPSVI', 'PSVI', pal[7]), ('SVI', 'SparseVI', pal[4]), ('RAND', 'Uniform', pal[3])]
+algs = [('BCORES', 'β-Cores', pal[0]), ('SVI', 'SparseVI', pal[4]), ('BPSVI', 'PSVI', pal[7]), ('RAND', 'Uniform', pal[3])] #
 fldr_figs = 'figs'
 if not os.path.exists(fldr_figs):
   os.mkdir(fldr_figs)
 
 figs=[]
 print('Plotting ' + dnm)
-fig = bkp.figure(y_axis_label='Accuracy',  x_axis_label='# Iterations', plot_height=1500, plot_width=2000, toolbar_location=None)
+fig = bkp.figure(y_axis_label='Predictive Accuracy',  x_axis_label='# Iterations', plot_height=1500, plot_width=2000, toolbar_location=None)
 preprocess_plot(fig, '72pt', False, False)
-fig2 = bkp.figure(y_axis_label='',  x_axis_label='Coreset Size', plot_height=1500, plot_width=2000, toolbar_location=None)
+fig2 = bkp.figure(y_axis_label='Predictive Accuracy',  x_axis_label='Coreset Size', plot_height=1500, plot_width=2000, toolbar_location=None)
 preprocess_plot(fig2, '72pt', False, False)
 fig3 = bkp.figure(y_axis_label='Negative Predictive Log-Likelihood',  x_axis_label='# Iterations', plot_height=1500, plot_width=2000, toolbar_location=None)
 preprocess_plot(fig3, '72pt', False, False)
-fig4 = bkp.figure(y_axis_label='',  x_axis_label='Coreset Size', plot_height=1500, plot_width=2000, toolbar_location=None)
+fig4 = bkp.figure(y_axis_label='Predictive LogLik',  x_axis_label='Coreset Size',   plot_height=1500, plot_width=2000, toolbar_location=None)
 preprocess_plot(fig4, '72pt', False, False)
 figs.append([fig, fig2, fig3, fig4])
 
 M=101
 
 for alg in algs:
+
   trials = [fn for fn in os.listdir(fldr_res) if fn.startswith(dnm+'_'+alg[0]+'_frate_'+str(f_rate)+'_i0_'+str(i0)+'_beta_'+str(beta)+'_graddiag_'+str(graddiag))]
+  print(trials)
   if len(trials) == 0:
     fig.line([], [], color=alg[2], legend_label=alg[1], line_width=10); fig.patch([], [], color=alg[2], legend_label=alg[1], alpha=0.3)
     fig2.line([], [], color=alg[2], legend_label=alg[1], line_width=10); fig2.patch([], [], color=alg[2], legend_label=alg[1], alpha=0.3)
@@ -53,7 +62,8 @@ for alg in algs:
     wts = res[0]
     pts = res[1]
     accs[tridx] = res[2]
-    plls[tridx]  = -res[3]
+    print(res[3])
+    plls[tridx]  = -np.log(-res[3])
     cszs[tridx, :] = np.array([len(w) for w in wts])
 
   csz50 = np.percentile(cszs, 50, axis=0)
@@ -63,6 +73,9 @@ for alg in algs:
   acc50 = np.percentile(accs, 50, axis=0)
   acc25 = np.percentile(accs, 25, axis=0)
   acc75 = np.percentile(accs, 75, axis=0)
+
+  print(np.mean(accs), np.std(accs))
+
 
   pll50 = np.percentile(plls, 50, axis=0)
   pll25 = np.percentile(plls, 25, axis=0)
@@ -87,22 +100,28 @@ for alg in algs:
 
 for f in [fig, fig3]:
   f.legend.location='top_left'
-  f.legend.label_text_font_size= '60pt'
+  f.legend.label_text_font_size= '80pt'
   f.legend.glyph_width=50
   f.legend.glyph_height=50
   f.legend.spacing=10
   f.legend.visible = True
 for f in [fig2, fig4]:
   f.legend.location='bottom_center'
-  f.legend.label_text_font_size= '60pt'
+  f.legend.label_text_font_size= '80pt'
   f.legend.glyph_width=50
   f.legend.glyph_height=50
   f.legend.spacing=10
   f.legend.visible = True
 
-export_png(fig, filename=os.path.join(fldr_figs, dnm+"_ACCvsit.png"), height=1500, width=2000)
-export_png(fig2, filename=os.path.join(fldr_figs, dnm+"_ACCvssz.png"), height=1500, width=2000)
-export_png(fig3, filename=os.path.join(fldr_figs, dnm+"_LLvsit.png"), height=1500, width=2000)
-export_png(fig4, filename=os.path.join(fldr_figs, dnm+"_LLvssz.png"), height=1500, width=2000)
+fig2.add_layout(Title(text=dnmnm, align="center", text_font='helvetica', text_font_style='bold', text_font_size = "80px"), "above")
 
-bkp.show(bkl.gridplot(figs))
+
+fnm = (str(beta)+'_'+str(i0)+'_'+str(f_rate)+'_'+str(graddiag)).replace('.', '')
+
+#export_png(fig, filename=os.path.join(fldr_figs, dnm+fnm+"_ACCvsit.png"), height=1500, width=2000)
+export_png(fig2, filename=os.path.join(fldr_figs, dnm+fnm+"_ACCvssz.png"), height=1500, width=2000)
+#export_png(fig3, filename=os.path.join(fldr_figs, dnm+fnm+"_LLvsit.png"), height=1500, width=2000)
+export_png(fig4, filename=os.path.join(fldr_figs, dnm+fnm+"_LLvssz.png"), height=1500, width=2000)
+
+np.savez('results/diagnostics_'+dnm+'_'+fnm, accs=accs, plls=plls)
+#bkp.show(bkl.gridplot(figs))
